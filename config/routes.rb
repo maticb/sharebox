bf803ccae3174d73f321073103f2d10cb2ed6688
Matic Blagus <matic.blagus@hotmail.com>
2013-02-25 09:54:37 +0100

    upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Sharebox::Application.routes.draw do
 
 
+  resources :assets
+
   devise_for :users
 
     root :to => "home#index"
-
+  match "assets/get/:id" => "assets#get", :as => "download"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
